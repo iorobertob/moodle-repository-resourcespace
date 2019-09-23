fcb08c82ae14b507fe981e674937315b04d439da
root <root@intermusic.lmta.lt>
2019-09-24 00:13:42 +0300

    first commit

---
+++ lang/en/repository_resourcespace.php
@@ -0,0 +1,17 @@
+<?php
+$string['configplugin'] = 'Configure ResourceSpace Repository';
+$string['pluginname'] = 'ResourceSpace';
+$string['pluginname_help'] = 'Resourcespace filepicker integration';
+
+$string['resourcespace_api_url'] = 'Resourcespace API URL';
+$string['resourcespace_api_url_help'] = 'The API URL is usually the full URL to your Resourcespace insallation followed by /api/?';
+$string['api_user'] = 'API user';
+$string['api_user_help'] = 'Enter the Resourcespace API user accountname';
+$string['api_key'] = 'Private API key';
+$string['api_key_help'] = 'The API key can be found in Resourcespaces userinfo';
+
+$string['enable_help'] = 'Show help button';
+$string['enable_help_help'] = 'This will display a help link in the filepicker';
+
+$string['enable_help_url'] = 'Help button url';
+$string['enable_help_url_help'] = 'Url the help button should link to';
---
+++ lib.php
@@ -0,0 +1,220 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * @package    repository_resourcespace
+ * @copyright  2018 Anders Jørgensen
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class repository_resourcespace extends repository {
+
+    public function __construct($repositoryid, $context, array $options, $readonly) {
+        parent::__construct($repositoryid, $context, $options, $readonly);
+        $this->config = get_config('resourcespace');
+        $this->resourcespace_api_url = get_config('resourcespace', 'resourcespace_api_url');
+	echo "<script>console.log('API URL: " . $this->resourcespace_api_url . "' );</script>";
+        $this->api_key = get_config('resourcespace', 'api_key');
+	echo "<script>console.log('API KEY: " . $this->api_key . "' );</script>";
+        $this->api_user = get_config('resourcespace', 'api_user');
+	echo "<script>console.log('API USER: " . $this->api_user . "' );</script>";
+        $this->enable_help = get_config('resourcespace', 'enable_help');
+        $this->enable_help_url = get_config('resourcespace', 'enable_help_url');
+    }
+
+    public function get_listing($path = '', $page = '') {
+        if ($path !== '') {
+            // Redirect to search, asking for filesa within the given collection
+	   echo "<script>console.log('REDIRECT');</script>";
+            return $this->search(sprintf('!collection%s', $path), $page);
+        }
+
+        $listArray = array(
+            'list' => $this->do_search_collections(),
+            'norefresh' => true,
+            'nologin' => true,
+            'dynload' => true,
+            'issearchresult' => false,
+        );
+
+        if ($this->enable_help == 1) {
+            $listArray['help'] = "$this->enable_help_url";
+        }
+
+        return $listArray;
+    }
+
+/*
+    public function print_search() {
+        $search = '<input class="form-control" id="reposearch" name="s" placeholder="Search" type="search">';
+        return $search;
+    }
+*/
+    public function search($searchText, $page = 0) {
+        $listArray = array(
+            'list' => array(),
+            'norefresh' => true,
+            'nologin' => true,
+            'dynload' => true,
+            'issearchresult' => true,
+        );
+
+        if ($this->enable_help == 1) {
+            $listArray['help'] = "$this->enable_help_url";
+        }
+
+        $collections = $this->do_search_collections($searchText);
+        $resources = $this->do_search_resources($searchText);
+
+        $listArray['list'] = array_merge($collections, $resources);
+
+        return $listArray;
+    }
+
+    public function get_file($url, $filename = '') {
+        // We have to catch the url, and make an additional request to the resourcespace api,
+        // to get the actual filesource.
+        $fileInfo = explode(',', $url);
+
+        $resourceUrl = $this->make_api_request('get_resource_path', array(
+            'param1' => $fileInfo[0], // $resource
+            'param2' => '0',          // $getfilepath
+            'param3' => '',           // $size
+            'param5' => $fileInfo[1], // $extension
+        ));
+
+        // Call the method of the parent class, then overwrite the URL back to what was passed to us
+        $result = parent::get_file($resourceUrl, $filename);
+        $result['url'] = $url;
+
+        return $result;
+    }
+
+    public function supported_filetypes() {
+        return '*';
+    }
+
+    public function global_search() {
+        return false;
+    }
+
+    public function supported_returntypes() {
+        return FILE_INTERNAL;
+    }
+
+    public static function get_type_option_names() {
+        return array_merge(parent::get_type_option_names(), array('resourcespace_api_url', 'api_user', 'api_key', 'enable_help', 'enable_help_url'));
+    }
+
+    public static function type_config_form($mform, $classname = 'repository') {
+        parent::type_config_form($mform);
+
+        $mform->addElement('html', '<hr>');
+        $mform->addElement('html', '<h2>Server settings</h2>');
+        $mform->addElement('text', 'resourcespace_api_url', get_string('resourcespace_api_url', 'repository_resourcespace'));
+        $mform->setType('resourcespace_api_url', PARAM_RAW_TRIMMED);
+        $mform->addRule('resourcespace_api_url', 'required', 'required', null, 'client');
+        $mform->addElement('static', null, '', get_string('resourcespace_api_url_help', 'repository_resourcespace'));
+
+        $mform->addElement('text', 'api_user', get_string('api_user', 'repository_resourcespace'));
+        $mform->setType('api_user', PARAM_RAW_TRIMMED);
+        $mform->addRule('api_user', '', 'required', null, 'client');
+        $mform->addElement('static', null, '', get_string('api_user_help', 'repository_resourcespace'));
+
+        $mform->addElement('password', 'api_key', get_string('api_key', 'repository_resourcespace'));
+        $mform->setType('api_key', PARAM_RAW_TRIMMED);
+        $mform->addRule('api_key', '', 'required', null, 'client');
+        $mform->addElement('static', null, '', get_string('api_key_help', 'repository_resourcespace'));
+
+        $mform->addElement('html', '<hr>');
+        $mform->addElement('html', '<h2>Miscellaneous settings</h2>');
+
+        $mform->addElement('checkbox', 'enable_help', get_string('enable_help', 'repository_resourcespace'));
+        $mform->addElement('static', null, '', get_string('enable_help_help', 'repository_resourcespace'));
+
+        $mform->addElement('text', 'enable_help_url', get_string('enable_help_url', 'repository_resourcespace'));
+        $mform->addElement('static', null, '', get_string('enable_help_url_help', 'repository_resourcespace'));
+    }
+
+    // Perform a search for collections and return a list of elements suitable for Moodle
+    protected function do_search_collections($searchText = '') {
+        $list = array();
+
+        $collections = $this->make_api_request('search_public_collections', array(
+            'param1' => $searchText,
+            // 'param2' => 'c.name',
+        ));
+
+        if (is_array($collections)) {
+            foreach ($collections as $collection) {
+                $list[] = array(
+                    'title' => $collection->name,
+                    'path' => $collection->ref,
+                    'date' => strtotime($collection->created),
+                    'children' => array(),
+                );
+            }
+        }
+
+        return $list;
+    }
+
+    // Perform a search for resources and return a list of elements suitable for Moodle
+    protected function do_search_resources($searchText = '') {
+        $list = array();
+
+        $resources = $this->make_api_request('search_get_previews', array(
+            'param1' => $searchText, // $search
+            // 'param3' => 'title',     // $order_by
+            'param5' => '-1',        // $fetchrows
+            'param8' => 'thm',       // $getsizes
+        ));
+
+        if (is_array($resources)) {
+            foreach ($resources as $resource) {
+                $list[] = array(
+                    'title' => $resource->field8,
+                    'thumbnail' => $resource->url_thm,
+                    // Parsing the resourcespace ref and file extension as the filesource, because the
+                    // resourcespace api does not return the actual source at this point.
+                    'source' => sprintf('%s,%s', $resource->ref, $resource->file_extension),
+                    'datemodified' => strtotime($resource->file_modified),
+                );
+            }
+        }
+
+        return $list;
+    }
+
+    // Make an API request
+    protected function make_api_request($method, $queryData) {
+        $queryData['user'] = $this->api_user;
+        $queryData['function'] = $method;
+
+        $query = http_build_query($queryData, '', '&');
+	echo "<script>console.log('QUERY: " . $query . "' );</script>";
+        // Sign the request with the private key.
+        $sign = hash("sha256", $this->api_key . $query);
+
+        $requestUrl = "$this->resourcespace_api_url" . $query . "&sign=" . $sign;
+	echo "<script>console.log('REQUEST URL: " . $requestUrl . "' );</script>";
+        $response = file_get_contents($requestUrl);
+
+        return json_decode($response);
+    }
+}
